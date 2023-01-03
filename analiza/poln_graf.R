8052e5aa28f801824ba9af385ac917de62f4dcf1
Nika Pavlič <nika.pavlic775@gmail.com>
2023-01-03 19:42:03 +0100

    presecisca

--- analiza/poln_graf.R
+++ analiza/poln_graf.R
@@ -7,16 +7,14 @@ library(stringr)
 
 #Naredi poln graf:
 
-
-poln_graf <- function(datoteka){
+poln_graf <- function(datoteka) {
   dat <- data.frame(fromJSON(file = datoteka))                     #prebere JSON
-  graf <- merge(dat,dat, by = NULL)                                #kartezicni produkt
-  colnames(graf) <- c("x1","y1","x2","y2")                        #spremeni imena
-  graf <- filter(graf, graf$x1!= graf$x2 & graf$y1 != graf$y2)
-  graf$temp <- apply(graf, 1, function(x) paste(sort(x), collapse=""))
-  graf <- graf[!duplicated(graf$temp), 1:4]                       #ostanejo daljice, ki se ne ponavljajo
+  dat <- mutate(dat, i = 1:nrow(dat))                              #doda indekse
+  graf <- merge(dat, dat, by = NULL)                               #kartezicni produkt
+  colnames(graf) <- c("x1", "y1", "i", "x2", "y2", "j")            #spremeni imena
+  graf <- filter(graf, i < j)
+  graf <- graf %>% select(-i, -j) %>% mutate(i = 1:nrow(graf))
 }
-
 
 
 # funkcija za leve zavoje:
@@ -33,10 +31,9 @@ naklon <- function(x1,y1,x2,y2){(y2-y1)/(x2-x1)}
 
 presecisca <- function(graf){
   pr <- merge(graf, graf, by = NULL)
-  colnames(pr) <- c("x1","y1","x2","y2","x3","y3","x4","y4")
-  pr$temp <- apply(pr, 1, function(x) paste(sort(x), collapse=""))
-  pr <- pr[!duplicated(pr$temp), 1:8]
-  pr <- filter(pr, !(pr$x1 == pr$x3 & pr$x2 == pr$x4))             #ostanejo nepodvojene kombinacije daljic, ki bi se lahko sekale
+  
+  colnames(pr) <- c("x1", "y1", "x2", "y2", "i", "x3", "y3", "x4", "y4", "j")
+  pr <- filter(pr, i < j)
   
   pr$Z123 <- Z(pr$x1,pr$y1,pr$x2,pr$y2, pr$x3,pr$y3)
   pr$Z124 <- Z(pr$x1,pr$y1,pr$x2,pr$y2, pr$x4,pr$y4)
@@ -54,12 +51,12 @@ presecisca <- function(graf){
 }
 
 
-
-graf <- poln_graf(datoteka = "podatki/tocke_10.json")
+graf <- poln_graf(datoteka = "podatki/tocke_5.json")
 c <-presecisca(graf)
 
 
-
+plot(c(graf$x1, graf$x2), c(graf$y1, graf$y2))
+segments(graf$x1, graf$y1, graf$x2, graf$y2)
 
 
 
@@ -71,13 +68,14 @@ pr <- function(datoteka){
   ob <- ifelse(str_extract_all(ime,"_{0,1}[krog]{4}")=="character(0)", "",str_extract_all(ime,"_{0,1}[krog]{4}"))
   graf <- poln_graf(datoteka)
   pr <- presecisca(graf)
-  pr <- pr[,14:17]
+  pr <- pr[,16:19]
   json <- toJSON(list(k1 = pr$k1, k2=pr$k2,X=pr$X,Y=pr$Y))
+  json
   write(json, str_glue("presecisca{ob}/presecisca_{ime}.json"))
 }
 
 
-pr("podatki/tocke_7.json")
+pr("podatki/tocke_5.json")
 pr("podatki_krog/tocke_7_krog.json")
 
 
